21a8d2bf3a58b71a0496c7c6915c95fecf9f6d73
Tumpa Biswas <tumpabiswas433@gmail.com>
2025-07-24 13:32:47 +0530

    admin index

---
+++ admin/index.php
@@ -0,0 +1,70 @@
+<?php
+require_once('inc/db_config.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login Panel</title>
+
+    <?php require('inc/links.php'); ?>
+
+    <style>
+        div.login-form {
+
+            position: absolute;
+
+            top: 50%;
+
+            left: 50%;
+
+            transform: translate(-50%, -50%);
+
+            width: 400px;
+
+        }
+    </style>
+
+</head>
+
+<body class="bg-light">
+
+
+    <div class="login-form text-center rounded bg-white shadow overflow-hidden">
+        <div class="p-4">
+            <h3 class="mb-4">Admin Login</h3>
+            <form method="post">
+                <div class="mb-3">
+                    <input name="admin" required type="text" name="username" class="form-control" placeholder="Username" required>
+                </div>
+                <div class="mb-3">
+                    <input name="pass" required type="password" name="password" class="form-control" placeholder="Password" required>
+                </div>
+                <button name="login" type="submit" class="btn btn-primary w-100">Login</button>
+            </form>
+        </div>
+
+        <div class="bg-dark text-white p-2">
+            <p class="mb-0">Admin Panel</p>
+        </div>
+    </div>
+
+
+<?php
+
+if(isset($_POST['login'])){
+    
+}
+
+?>
+
+
+
+    <?php require('inc/scripts.php') ?>
+
+
+</body>
+
+</html>
